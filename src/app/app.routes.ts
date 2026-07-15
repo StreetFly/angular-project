54808331dadb054452d27088294c3f801b2a861e
StreetFly <ryannitroo@gmail.com>
2026-07-15 13:23:27 -0700

    catching up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomePage } from '../pages/home/home.component';
+import { AboutPage } from '../pages/about/about';
+import { AdminPage } from '../pages/admin/admin';
+import { LoginPage } from '../pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePage
+    },
+    { 
+        path: 'about',
+        component: AboutPage
+    },
+    {
+        path: 'admin',
+        component: AdminPage
+    },
+    {
+        path: 'login',
+        component: LoginPage
+    }
+];
